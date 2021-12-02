d60838f72930adc9aa4f154fbbf35c25f64a144b
George Vince <george.vince@hotmail.co.uk>
2021-12-02 14:38:08 +0000

    day 1: part 2

--- day-1.py
+++ day-1.py
@@ -1,9 +1,11 @@
 from typing import List
 
+
 def get_input() -> List[int]:
-    with open('input/day-1.txt') as f:
+    with open("input/day-1.txt") as f:
         contents = f.readlines()
     return [int(x) for x in contents]
+
 
 def part_1(input: List[int]) -> int:
     count = 0
@@ -15,7 +17,19 @@ def part_1(input: List[int]) -> int:
     return count
 
 
+def part_2(input: List[int]) -> int:
+    count = 0
+    cur_window = sum(input[0:3])
+    for indx in range(1, len(input) - 1):
+        next_window = sum(input[indx : indx + 3])
+        if next_window > cur_window:
+            count += 1
+        cur_window = next_window
+    return count
+
+
 if __name__ == "__main__":
     input = get_input()
     print(part_1(input))
-    
+    print(part_2(input))
+
