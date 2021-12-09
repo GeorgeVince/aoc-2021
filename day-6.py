98b409c3776b049298b44fc0c8cb9b4dbd27f2b2
George <george.vince@hotmail.co.uk>
2021-12-09 13:54:57 +0000

    added: day 6 part 1

---
+++ day-6.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class LanternFish:
+    time_left: int = 8
+    first_cycle: bool = True
+
+    def add_day(self):
+        self.time_left -= 1
+        if self.time_left < 0:
+            self.time_left = 6
+            self.first_cycle = False
+            return LanternFish()
+
+
+def get_input() -> int:
+    with open("./input/day-6.txt", 'r') as f:
+        contents = f.read()
+    return [int(x) for x in contents.split(",")]
+
+
+def part_one(inital_ages: List[int]) -> int:
+    fishes = []
+    num_days = 80
+
+    for age in inital_ages:
+        fishes.append(LanternFish(time_left=age, first_cycle=False))
+
+    for _ in range(0, num_days):
+        new_fishes = []
+        for fish in fishes:
+            new_fish = fish.add_day()
+            if new_fish:
+                new_fishes.append(new_fish)
+        fishes.extend(new_fishes)
+
+    return len(fishes)
+
+
+if __name__ == "__main__":
+    input = get_input()
+    print(part_one(input))
