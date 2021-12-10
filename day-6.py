22a80a6fdfb3a92517da7b0f2a5ad1487b6b9207
George <george.vince@hotmail.co.uk>
2021-12-10 14:08:59 +0000

    tidy: day 6

--- day-6.py
+++ day-6.py
@@ -2,58 +2,32 @@ from dataclasses import dataclass
 from typing import Counter, List
 
 
-@dataclass
-class LanternFish:
-    time_left: int = 8
-    first_cycle: bool = True
-
-    def add_day(self):
-        self.time_left -= 1
-        if self.time_left < 0:
-            self.time_left = 6
-            self.first_cycle = False
-            return LanternFish()
-
-
 def get_input() -> int:
     with open("./input/day-6.txt", 'r') as f:
         contents = f.read()
     return [int(x) for x in contents.split(",")]
 
 
-def simulate(inital_ages: List[int], num_days: int) -> int:
-    fishes = []
-
-    for age in inital_ages:
-        fishes.append(LanternFish(time_left=age, first_cycle=False))
+def simulate(initial_ages: List[int], num_days: int) -> int:
+    age_occurances = {day_val: initial_ages.count(
+        day_val) for day_val in range(-1, 9)}
 
     for _ in range(0, num_days):
-        new_fishes = []
-        for fish in fishes:
-            new_fish = fish.add_day()
-            if new_fish:
-                new_fishes.append(new_fish)
-        fishes.extend(new_fishes)
-    return len(fishes)
+        age_occurances = {k-1: v for k, v in age_occurances.items() if k >= 0}
 
+        age_occurances[8] = age_occurances.get(-1, 0)
+        age_occurances[6] += age_occurances.get(-1, 0)
+        age_occurances[-1] = 0
 
-def part_one(inital_ages: List[int]) -> int:
-    return simulate(inital_ages, 80)
+    return sum(age_occurances.values())
 
+def part_one(input: List[int], num_days: int) -> int:
+    return simulate(input, 80)
 
-def part_two(inital_ages: List[int]) -> int:
-    # This won't work so have to be a bit more clever!
-    # return simulate(inital_ages, 256)
-    
-    counts = [inital_ages.count(i) for i in range(0, 9)]
-    for day in range(0, 256):
-        counts[(day + 7) % 9] += counts[day % 9]
-    
-    return sum(counts)
-
-
+def part_two(input: List[int], num_days: int) -> int:
+    return simulate(input, 256)
 
 if __name__ == "__main__":
     input = get_input()
-    print(part_one(input))
-    print(part_two(input))
+    print(part_one(input, 80))
+    print(part_two(input, 256))
