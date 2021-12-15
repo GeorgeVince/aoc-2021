74125636d2abe5351ada5a1cf9bfb545224e029f
George <george.vince@hotmail.co.uk>
2021-12-15 12:37:37 +0000

    added: day 13 part 2

--- day-13.py
+++ day-13.py
@@ -1,7 +1,10 @@
+from os import X_OK
+
+
 def get_input():
     with open("./input/day-13.txt", 'r') as f:
         contents = f.readlines()
-    
+
     coords = []
     folds = []
     for line in contents:
@@ -13,32 +16,47 @@ def get_input():
             folds.append([split[0][-1], int(split[1])])
     return coords, folds
 
+
 def _fold(coord, fold):
     if fold[0] == "x":
-            if coord[0] > fold[1]:
-                coord[0] = fold[1] - (coord[0] - fold[1])
+        if coord[0] > fold[1]:
+            coord[0] = fold[1] - (coord[0] - fold[1])
     if fold[0] == "y":
         if coord[1] > fold[1]:
             coord[1] = fold[1] - (coord[1] - fold[1])
     return coord
-        
+
 
 def part_one(coords, folds):
     for coord in coords:
         coord = _fold(coord, folds[0])
+    print(coords)
+    return len(set([(*x,) for x in coords]))
+
+
+def draw(coords):
+    max_x = max([x[0] for x in coords])
+    max_y = max([y[1] for y in coords])
     
-    return len (set([(*x,) for x in coords]))
+    drawn = []
+    for _ in range(0, max_y + 1):
+        drawn.append(["." for _ in range(0, max_x + 1)])
+
+    for coord in coords:
+        drawn[coord[1]][coord[0]] = "#"
+        for row in drawn:
+            print("".join(row))
+        print("")
 
 def part_two(coords, folds):
     for fold in folds:
         for coord in coords:
             coord = _fold(coord, fold)
     
-    return len (set([(*x,) for x in coords]))
-
+    draw(coords)
 
 if __name__ == "__main__":
     input = get_input()
-    print(input)
-    print(part_one(*input))
+    # print(input)
+    # print(part_one(*input))
     print(part_two(*input))
